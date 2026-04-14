9001ffb1fcaa7a91e6a93d047483409eff405a05
vishnu <iamvishnukrv@gmail.com>
2026-04-14 18:01:58 +0530

    ami_id name changed

--- environments/qa/main.tf
+++ environments/qa/main.tf
@@ -8,7 +8,7 @@ module "Vpc" {
 
 module "EC2" {
   source            = "../../modules/EC2"
-  ami_id            = "ami-03c3282f979a6a9b0"
+  ami_id            = "ami-0a1b6a02658659c2a"
   instance_type     = "t3.micro"
   subnet_id         = module.Vpc.subnet_id
   ec2_count         = 1
