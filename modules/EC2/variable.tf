1d9d6d98b5e1badf03feb2b3245db1f587f1d940
vishnu <iamvishnukrv@gmail.com>
2026-04-14 10:30:48 +0530

    Added modules in master branch

--- modules/EC2/variable.tf
+++ modules/EC2/variable.tf
@@ -1,24 +1,24 @@
 variable "ami_id" {
   type        = string
-  description = "ami_id of EC2"
+  description = "Ami id of my EC2"
 }
 
 variable "instance_type" {
   type        = string
-  description = "Instance type of my EC2"
+  description = "Instance Type of my EC2"
 }
 
 variable "subnet_id" {
   type        = string
-  description = "subnet_id of my EC2"
+  description = "Subnet ID of my EC2"
 }
 
-variable "EC2_count" {
+variable "ec2_count" {
   type        = string
-  description = "No of EC2 to be created"
+  description = "No of my EC2 to be created"
 }
 
 variable "env" {
   type        = string
-  description = "what env we are creating"
+  description = "What Env we are creating (dev, qa, production)"
 }
