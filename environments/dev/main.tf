98177bddc590d434ba134091019526c37cc08ecd
vishnu <iamvishnukrv@gmail.com>
2026-04-14 15:21:56 +0530

    module names changed in source

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,6 @@
 
 module "Vpc" {
-  source        = "../../modules/vpc"
+  source        = "../../modules/Vpc"
   vpc_cidr      = "10.0.0.0/16"
   subnet_cidr   = "10.0.1.0/24"
   subnet_az     = "${var.aws_region}a"          # ap-southeast-1a
@@ -8,7 +8,7 @@ module "Vpc" {
 }
 
 module "EC2" {
-  source            = "../../modules/ec2"
+  source            = "../../modules/EC2"
   ami_id            = "ami-0afc7fe9be84307e4"
   instance_type     = "t2.micro"
   subnet_id         = module.vpc.subnet_id
