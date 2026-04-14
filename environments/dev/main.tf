0cc107d86f75e1b115a1af9860fe3700a9741262
vishnu <iamvishnukrv@gmail.com>
2026-04-14 16:30:53 +0530

    subnet name changed in EC2 module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,7 +11,7 @@ module "EC2" {
   source            = "../../modules/EC2"
   ami_id            = "ami-0afc7fe9be84307e4"
   instance_type     = "t2.micro"
-  subnet_id         = module.vpc.subnet_id
+  subnet_id         = module.Vpc.subnet_id
   ec2_count         = 2
   env               = "dev"
 }
