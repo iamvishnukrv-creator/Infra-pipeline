1f125e690bc7204c0130193d69b93e99873630c9
vishnu <iamvishnukrv@gmail.com>
2026-04-14 16:49:56 +0530

    ami  name change in EC2 module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -9,7 +9,7 @@ module "Vpc" {
 
 module "EC2" {
   source            = "../../modules/EC2"
-  ami_id            = "ami-0a1ed9012b6b5aac7"
+  ami_id            = "ami-03c3282f979a6a9b0"
   instance_type     = "t2.micro"
   subnet_id         = module.Vpc.subnet_id
   ec2_count         = 2
